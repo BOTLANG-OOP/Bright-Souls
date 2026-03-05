8bf34d2b4818e05120dc6dd9c0ad13d282d3e41a
Hynes <hynesgavin.daugdaug@cit.edu>
2026-03-06 00:20:44 +0800

    Implemented Dragon, Goblin, and Skeleton enemy classes

--- rpg-battle-system/src/main/java/com/rpg/enemies/Skeleton.java
+++ rpg-battle-system/src/main/java/com/rpg/enemies/Skeleton.java
@@ -1,10 +1,31 @@
 /**
- * Author: TBD
+ * Author: Hynes
  */
 package com.rpg.enemies;
 
 import com.rpg.core.Enemy;
+import com.rpg.core.Character;
 
 public class Skeleton extends Enemy {
+
+    public Skeleton(){
+        super("Skrall", 90, 30, 10, 50, 30);
+    }
+
+    @Override
+    public void performAttack(Character target) {
+        int damage = (int)(getAttack());
+
+        System.out.println("Skrall shot "+target.getName()+" with an arrow! for "+damage+" damage!");
+        target.takeDamage(damage);
+
+        Regenerate();
+    }
+
+    public void Regenerate(){
+       setHp(getHp()+5);
+       System.out.println("Skrall regenerates 5 HP!");
+    }
+
 
 }
