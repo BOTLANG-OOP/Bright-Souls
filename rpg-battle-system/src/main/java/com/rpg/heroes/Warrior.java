279ae39917859abc9c7cd308e46bfd0c3e27576e
johnkevinrepolidon <johnkevinrepolidon@gmail.com>
2026-03-06 22:13:06 +0800

    Warrior Update
    Added attack and ability

--- rpg-battle-system/src/main/java/com/rpg/heroes/Warrior.java
+++ rpg-battle-system/src/main/java/com/rpg/heroes/Warrior.java
@@ -6,5 +6,18 @@ package com.rpg.heroes;
 import com.rpg.core.Hero;
 
 public class Warrior extends Hero {
+    public Warrior(String n, int h, int a) {
+        super(n, h, a);
+    }
 
+    @Override
+    public void performAttack() {
+        System.out.println(getName() + " swings a heavy sword!");
+    }
+
+    @Override
+    public void useSpecialAbility() {
+        System.out.println(getName() + " uses Shield Bash! The enemy is stunned and skips their next turn.");
+    }
 }
+  
