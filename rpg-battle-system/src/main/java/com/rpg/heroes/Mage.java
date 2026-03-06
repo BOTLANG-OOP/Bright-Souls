4c61e6aa472fd7c0c12315295938938840bc440f
johnkevinrepolidon <johnkevinrepolidon@gmail.com>
2026-03-06 22:12:09 +0800

    Update Mage.java
    Added attack and abilities

--- rpg-battle-system/src/main/java/com/rpg/heroes/Mage.java
+++ rpg-battle-system/src/main/java/com/rpg/heroes/Mage.java
@@ -6,5 +6,18 @@ package com.rpg.heroes;
 import com.rpg.core.Hero;
 
 public class Mage extends Hero {
+    public Mage(String n, int h, int a) {
+        super(n, h, a);
+    }
 
+    @Override
+    public void performAttack() {
+        System.out.println(getName() + " casts a magic bolt!");
+    }
+
+    @Override
+    public void useSpecialAbility() {
+        double d = getAttack() * 1.5;
+        System.out.println(getName() + " casts Fireball for " + d + " damage!");
+    }
 }
