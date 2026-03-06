204c0fddadfda30237bdb06aad7dbe4eb7abbae8
johnkevinrepolidon <johnkevinrepolidon@gmail.com>
2026-03-06 22:09:38 +0800

    Add Archer class with attack and special ability methods
    Added abilities with special ability and attack options

--- rpg-battle-system/src/main/java/com/rpg/heroes/Archer.java
+++ rpg-battle-system/src/main/java/com/rpg/heroes/Archer.java
@@ -6,5 +6,19 @@ package com.rpg.heroes;
 import com.rpg.core.Hero;
 
 public class Archer extends Hero {
+    public Archer(String n, int h, int a) {
+        super(n, h, a);
+    }
 
+    @Override
+    public void performAttack() {
+        System.out.println(getName() + " fires a steady shot!");
+    }
+
+    @Override
+    public void useSpecialAbility() {
+        double d = getAttack() * 0.75;
+        System.out.println(getName() + " uses Double Shot for " + d + " damage twice!");
+    }
 }
+
